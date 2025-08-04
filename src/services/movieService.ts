32a8265777b7ebce0949af4ce71e350f9667e76b
Vita Inhulska <gavrishenko.vita@gmail.com>
2025-08-04 20:39:43 +0300

    fixed movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,11 +3,13 @@ import type { Movie } from "../types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
+interface MovieResponse {
+  results: Movie[];
+}
+
 export async function fetchMovies(query: string): Promise<Movie[]> {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      query,
-    },
+  const response = await axios.get<MovieResponse>(BASE_URL, {
+    params: { query },
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
